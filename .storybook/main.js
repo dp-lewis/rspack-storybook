7102fb3156d22aae05dfaddde36ebc861e4252f2
David Lewis <mail@david-lewis.com>
2025-07-18 19:53:52 +1000

    refactor: remove Header, Page components and their associated stories and styles
    - Deleted Header component and its story, along with related styles.
    - Removed Page component and its story, including associated styles.
    - Cleaned up assets by deleting unused images and SVGs.
    - Introduced rsbuild configuration for project setup.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,15 @@
-
-
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-docs"
+  addons: [
+    '@storybook/addon-docs'
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: 'storybook-react-rsbuild',
+    options: {}
   }
 };
+
 export default config;
