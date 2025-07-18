a3e4da3e1deec62265c6772a7b1ba44dcb24083c
David Lewis <mail@david-lewis.com>
2025-07-19 08:34:32 +1000

    feat: add dark theme support with new theme-dark.css and update Storybook configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import '../src/tokens/design-tokens.css';
+import '../src/tokens/semantic-tokens.css';
+import '../src/tokens/theme-dark.css';
+
 /** @type { import('@storybook/react-webpack5').Preview } */
 const preview = {
   parameters: {
@@ -6,6 +10,14 @@ const preview = {
        color: /(background|color)$/i,
        date: /Date$/i,
       },
+    },
+    themes: {
+      default: 'Light',
+      list: [
+        { name: 'Light', class: '', color: '#fff', default: true },
+        { name: 'Dark', class: 'theme-dark', color: '#222' }
+      ],
+      target: 'body',
     },
   },
 };
