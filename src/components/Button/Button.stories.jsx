7c42e024048ca260d082ac5a2f5e0e627696f147
David Lewis <mail@david-lewis.com>
2025-07-18 20:13:04 +1000

    feat: implement design tokens for button styles and add foundational and semantic token files

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button } from "./Button";
+import '../../tokens/foundations.css';
+import '../../tokens/semantic-tokens.css';
 
 export default {
   title: "Example/Button",
