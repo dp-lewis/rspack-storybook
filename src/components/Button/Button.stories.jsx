65506a5dab42b57a6f1a5377b1dc7b7a334903f7
David Lewis <mail@david-lewis.com>
2025-07-19 08:27:52 +1000

    feat: consolidate design tokens by removing foundations.css and updating semantic tokens

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Button } from "./Button";
-import '../../tokens/foundations.css';
+import '../../tokens/design-tokens.css';
 import '../../tokens/semantic-tokens.css';
+import './component-tokens.css';
 
 export default {
   title: "Example/Button",
