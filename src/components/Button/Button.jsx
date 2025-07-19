50f3f2dcfa9a2f0920d167ac2116e411069c0bca
David Lewis <mail@david-lewis.com>
2025-07-19 21:17:20 +1000

    refactor: remove PropTypes from Button component and update stories for clarity

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 export function Button({ label, onClick, type = "button" }) {
   return (
@@ -9,8 +8,3 @@ export function Button({ label, onClick, type = "button" }) {
   );
 }
 
-Button.propTypes = {
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-  type: PropTypes.oneOf(["button", "submit", "reset"]),
-};
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { Button } from "./Button";
-n
+
 export default {
-  title: "Components/Button",
+  title: "Example/Button",
   component: Button,
-  argTypes: {
-    label: { control: "text" },
-    onClick: { action: "clicked" },
-    type: {
-      control: { type: "select" },
-      options: ["button", "submit", "reset"],
-    },
+  tags: ["autodocs"],
+};
+
+export const Primary = {
+  args: {
+    label: "Click me!",
   },
 };
 
-const Template = (args) => <Button {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-  label: "Click Me",
-  type: "button",
-};
